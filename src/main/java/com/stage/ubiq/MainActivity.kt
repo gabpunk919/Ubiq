0173e462bf6d694f53820ec98da7b96ab57fcb27
gabpunk919 <g.boivert@gmail.com>
2023-03-31 09:39:00 -0400

    arranger nav plus bouton

--- src/main/java/com/stage/ubiq/MainActivity.kt
+++ src/main/java/com/stage/ubiq/MainActivity.kt
@@ -2,10 +2,26 @@ package com.stage.ubiq
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.R
+import com.stage.ubiq.databinding.ActivityMainBinding
+import com.stage.ubiq.databinding.FragmentAccueilBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var vue: FragmentAccueilBinding
+    private lateinit var vue2: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        vue = FragmentAccueilBinding.inflate(layoutInflater)
+
+        setContentView(vue.root)
+
     }
+
 }
